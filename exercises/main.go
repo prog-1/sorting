c7a2173dc57d79d39cafad95c3f900444c0e8cca
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-09-16 23:51:17 +0300

    not completed bisect yet

--- exercises/main.go
+++ exercises/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func insertSort(x []int) {
 	for i := 1; i < len(x); i++ {
 		for j := i; j > 0; j-- {
@@ -9,6 +11,31 @@ func insertSort(x []int) {
 		}
 	}
 }
+
+func findInsertionIndex(x []int, a int) int {
+	i, j := 0, len(x)-1
+
+	for i = 0; i <= j; j-- {
+		mid := i + j>>1
+		fmt.Println(mid)
+		if a < x[mid] {
+			j = mid - 1
+		} else {
+			i = mid + 1
+		}
+	}
+	return i
+}
+func insertBisSort(x []int) {
+	for i := 1; i < len(x); i++ {
+		indx := findInsertionIndex(x[0:i], x[i])
+		temp := x[i]
+		for j := i; j > indx; j-- {
+			x[j] = x[j-1]
+		}
+		x[indx] = temp
+	}
+} //bisect insertion sort scetch
 
 //Found this quite interesting piece on https://medium.com/star-gazers/insertion-sorting-algorithm-in-go-26f1ec49936c
 //It was written on base of pseudocode
@@ -40,9 +67,7 @@ func selectionSort(x []int) {
 }
 
 func main() {
-	// what is the most important in sorting algorythms?
-	// How to know which is more effective?°
-	//BenchmarkInsertSort-2      	 2295277	       530.6 ns/op	       0 B/op	       0 allocs/op
-	//BenchmarkInsertions-2      	 8224591	       147.5 ns/op	       0 B/op	       0 allocs/op
-	//BenchmarkSelectionSort-2   	 1417420	       839.0 ns/op	       0 B/op	       0 allocs/op
+	x := []int{5, 9, 7, 0, 3}
+	insertBisSort(x)
+	fmt.Println(x)
 }
